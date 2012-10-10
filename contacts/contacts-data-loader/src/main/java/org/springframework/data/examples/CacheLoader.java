0558870a0739be649e9a7d9ff464dd793328b3be
David Turanski <dturanski_at_vmware_dot_com>
2012-10-10 17:18:41 -0400

    more clean up

---
+++ contacts/contacts-data-loader/src/main/java/org/springframework/data/examples/CacheLoader.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2012 by the original author(s).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.examples;
+
+import java.util.HashSet;
+
+import javax.annotation.Resource;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.data.examples.domain.Contact;
+import org.springframework.data.examples.repository.ContactRepository;
+import org.springframework.stereotype.Component;
+
+import com.gemstone.gemfire.cache.Region;
+/**
+ * Migrates data from a source repository to a GemFire Region
+ * 
+ * @author David Turanski
+ *
+ */
+@Component
+public class CacheLoader {
+	/**
+	 * Migrates data from RDBMS to Cache
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext(
+				"META-INF/spring/applicationContext.xml");
+		CacheLoader cacheLoader = ctx.getBean(CacheLoader.class);
+		cacheLoader.loadData();
+	}
+	
+	
+	private static Log log = LogFactory.getLog(CacheLoader.class);
+	@Autowired 
+	ContactRepository contactRepository;
+	
+	@Resource(name="Contact")
+	Region<Long,Contact> contactRegion;
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public void loadData() {
+		log.info("retrieving contacts from Source Repository...");
+		Iterable<Contact> contacts = contactRepository.findAll();
+		log.info("loading cache...");
+	 
+		
+		for (Contact contact: contacts ) {
+			contactRegion.remove(contact.getId());
+			//Remove hibernate persistent set
+			contact.setPhones(new HashSet(contact.getPhones()));
+			contact.setEmailAddresses(new HashSet(contact.getEmailAddresses()));
+			contactRegion.put(contact.getId(),contact);
+		}
+		log.info("loaded " + contactRepository.count()+ " items into cache");
+	}
+
+}
---
+++ contacts/contacts-data-loader/src/main/java/org/springframework/data/examples/DataGenerator.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2012 by the original author(s).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.examples;
+
+import java.util.Date;
+import java.util.Random;
+
+import org.springframework.data.examples.domain.EmailAddress;
+import org.springframework.data.examples.domain.Phone;
+import org.springframework.util.StringUtils;
+
+public class DataGenerator {
+
+	private Random rand;
+	private String[] firstNames;
+	private String[] lastNames;
+	private String[] emailDomains;
+
+	public DataGenerator(String[] firstNames, String[] lastNames,
+			String[] emailDomains) {
+		rand = new Random(new Date().getTime());
+		this.firstNames = firstNames;
+		this.lastNames = lastNames;
+		this.emailDomains = emailDomains;
+	}
+
+	public String randomFirstName() {
+		return getRandomData(firstNames);
+	}
+
+	public String randomLastName() {
+		return getRandomData(lastNames);
+	}
+
+	public EmailAddress randomEmailAddress(String firstName, String lastName) {
+
+		lastName = lastName.replaceAll("\\W", "").toLowerCase();
+		String principal = firstName.toLowerCase() + "." + lastName;
+
+		boolean useLastname = rand.nextInt(10) == 0;
+
+		String domain = useLastname ? lastName + ".com"
+				: getRandomData(emailDomains);
+		return new EmailAddress(principal + "@" + domain);
+	}
+
+	public EmailAddress companyEmailAddress(String firstName, String lastName,
+			String companyName) {
+
+		lastName = lastName.replaceAll("\\W", "").toLowerCase();
+
+		String principal = firstName.toLowerCase().substring(0, 1) + lastName;
+
+		companyName = companyName.replaceAll("\\W", "").toLowerCase();
+		String domain = companyName + ".com";
+		return new EmailAddress(principal + "@" + domain);
+	}
+
+	public Phone getRandomPhone() {
+
+		String countryCode = String.format("%03d", rand.nextInt(1000));
+
+		String areaCode = String.format("%3d", rand.nextInt(800) + 200);
+
+		String prefix = String.format("%3d", rand.nextInt(900) + 100);
+
+		String number = String.format("%04d", rand.nextInt(1000));
+
+		String phoneNumber = StringUtils.arrayToDelimitedString(new Object[] {
+				countryCode, areaCode, prefix, number }, "-");
+
+		Phone.Type[] phoneTypes = new Phone.Type[] { Phone.Type.FAX,
+				Phone.Type.MOBILE, Phone.Type.OFFICE, Phone.Type.HOME };
+
+		int i = rand.nextInt(phoneTypes.length);
+
+		return new Phone(phoneNumber, phoneTypes[i]);
+	}
+
+	private String getRandomData(String[] data) {
+		int i = rand.nextInt(data.length);
+		return data[i];
+	}
+}
